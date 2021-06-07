1a6e6bd8f22f952e22fb85bd13f661c0ba3b8bc6
zjun <565061763@qq.com>
2021-06-07 18:31:36 +0800

    refactor

--- lib/src/main/java/com/sd/lib/umeng_share/provider/LibUmengShareFileProvider.java
+++ lib/src/main/java/com/sd/lib/umeng_share/provider/LibUmengShareFileProvider.java
@@ -1,7 +1,8 @@
 package com.sd.lib.umeng_share.provider;
 
 import android.content.Context;
-import android.support.v4.content.FileProvider;
+
+import androidx.core.content.FileProvider;
 
 public class LibUmengShareFileProvider extends FileProvider
 {
