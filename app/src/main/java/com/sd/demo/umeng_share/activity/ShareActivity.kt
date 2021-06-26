b2d5c4f018ae9910067eacb9a37d1ef9f50e752b
zjun <565061763@qq.com>
2021-06-26 17:10:15 +0800

    ShareActivity

--- app/src/main/java/com/sd/demo/umeng_share/activity/ShareActivity.kt
+++ app/src/main/java/com/sd/demo/umeng_share/activity/ShareActivity.kt
@@ -1,9 +1,14 @@
 package com.sd.demo.umeng_share.activity
 
 import android.os.Bundle
+import com.sd.demo.umeng_share.databinding.ActivityShareBinding
 
 class ShareActivity : BaseActivity() {
+    private lateinit var _binding: ActivityShareBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        _binding = ActivityShareBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
     }
 }
