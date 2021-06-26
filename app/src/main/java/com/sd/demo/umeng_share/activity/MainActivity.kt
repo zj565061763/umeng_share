0b9243cd459026837a81927702ec6a86bb1c1c96
zjun <565061763@qq.com>
2021-06-26 14:29:56 +0800

    MainActivity

--- app/src/main/java/com/sd/demo/umeng_share/activity/MainActivity.kt
+++ app/src/main/java/com/sd/demo/umeng_share/activity/MainActivity.kt
@@ -1,23 +1,19 @@
-package com.sd.demo.umeng_share.activity;
+package com.sd.demo.umeng_share.activity
 
-import android.content.Intent;
-import android.os.Bundle;
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sd.demo.umeng_share.R
+import com.umeng.socialize.UMShareAPI
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.sd.demo.umeng_share.R;
-import com.umeng.socialize.UMShareAPI;
-
-public class MainActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        UMShareAPI.get(this).onActivityResult(requestCode, resultCode, data);
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        UMShareAPI.get(this).onActivityResult(requestCode, resultCode, data)
     }
 }
