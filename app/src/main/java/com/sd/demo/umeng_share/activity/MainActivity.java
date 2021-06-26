fb19f2aaede58d4be06fdb1e98f44fa4fcb01468
zjun <565061763@qq.com>
2021-06-26 14:29:21 +0800

    demo

--- app/src/main/java/com/sd/demo/umeng_share/activity/MainActivity.java
+++ app/src/main/java/com/sd/demo/umeng_share/activity/MainActivity.java
@@ -1,15 +1,23 @@
 package com.sd.demo.umeng_share.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.sd.demo.umeng_share.R;
+import com.umeng.socialize.UMShareAPI;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        UMShareAPI.get(this).onActivityResult(requestCode, resultCode, data);
     }
 }
