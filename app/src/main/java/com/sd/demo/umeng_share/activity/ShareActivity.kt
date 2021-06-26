6e3fd115180792a2c6ac3cea20558a3c33dbfdb5
zjun <565061763@qq.com>
2021-06-26 17:11:43 +0800

    ShareActivity

--- app/src/main/java/com/sd/demo/umeng_share/activity/ShareActivity.kt
+++ app/src/main/java/com/sd/demo/umeng_share/activity/ShareActivity.kt
@@ -1,14 +1,35 @@
 package com.sd.demo.umeng_share.activity
 
 import android.os.Bundle
+import android.view.View
 import com.sd.demo.umeng_share.databinding.ActivityShareBinding
 
-class ShareActivity : BaseActivity() {
+class ShareActivity : BaseActivity(), View.OnClickListener {
     private lateinit var _binding: ActivityShareBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityShareBinding.inflate(layoutInflater)
         setContentView(_binding.root)
+    }
+
+    override fun onClick(v: View?) {
+        when (v) {
+            _binding.btnShareQq -> shareQQ()
+            _binding.btnShareWechat -> shareWechat()
+            _binding.btnShareSina -> shareSina()
+        }
+    }
+
+    private fun shareQQ() {
+
+    }
+
+    private fun shareWechat() {
+
+    }
+
+    private fun shareSina() {
+
     }
 }
