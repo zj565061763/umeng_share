5c6f366f5198878c7607713d3645438f347b1efc
zjun <565061763@qq.com>
2021-06-26 14:59:59 +0800

    Log

--- app/src/main/java/com/sd/demo/umeng_share/activity/MainActivity.kt
+++ app/src/main/java/com/sd/demo/umeng_share/activity/MainActivity.kt
@@ -2,16 +2,21 @@ package com.sd.demo.umeng_share.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.sd.demo.umeng_share.databinding.ActivityMainBinding
 import com.sd.lib.umeng_share.LibUmengShare
 import com.umeng.commonsdk.UMConfigure
 import com.umeng.socialize.UMAuthListener
 import com.umeng.socialize.UMShareAPI
+import com.umeng.socialize.UMShareConfig
 import com.umeng.socialize.bean.SHARE_MEDIA
 
+
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private val TAG = MainActivity::class.java.simpleName
     private lateinit var _binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,22 +31,17 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 UMConfigure.DEVICE_TYPE_PHONE,
                 "669c30a9584623e70e8cd01b0381dcb4")
 
-        LibUmengShare.init(this)
-    }
+        val config = UMShareConfig()
+        config.isNeedAuthOnGetUserInfo(true)
+        UMShareAPI.get(this).setShareConfig(config)
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        UMShareAPI.get(this).onActivityResult(requestCode, resultCode, data)
+        LibUmengShare.init(this)
     }
 
     override fun onClick(v: View?) {
         when (v) {
-            _binding.btnLoginQq -> {
-
-            }
-            _binding.btnLoginWechat -> {
-
-            }
+            _binding.btnLoginQq -> loginQQ()
+            _binding.btnLoginWechat -> loginWechat()
             _binding.btnLoginSina -> {
 
             }
@@ -49,19 +49,57 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     /**
+     * QQ登录
+     */
+    private fun loginQQ() {
+        val shareAPI = UMShareAPI.get(this)
+        val isInstall = shareAPI.isInstall(this, SHARE_MEDIA.QQ)
+        if (!isInstall) {
+            Toast.makeText(this, "QQ未安装", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        shareAPI.getPlatformInfo(this, SHARE_MEDIA.QQ, _umAuthListener)
+    }
+
+    /**
+     * 微信登录
+     */
+    private fun loginWechat() {
+        val shareAPI = UMShareAPI.get(this)
+        val isInstall = shareAPI.isInstall(this, SHARE_MEDIA.WEIXIN)
+        if (!isInstall) {
+            Toast.makeText(this, "微信未安装", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        shareAPI.getPlatformInfo(this, SHARE_MEDIA.WEIXIN, _umAuthListener)
+    }
+
+    /**
      * 授权监听
      */
     private val _umAuthListener = object : UMAuthListener {
         override fun onStart(platform: SHARE_MEDIA) {
+            Log.i(TAG, "onStart ${platform}")
         }
 
         override fun onComplete(platform: SHARE_MEDIA, action: Int, data: MutableMap<String, String>) {
+            Log.i(TAG, "onComplete ${platform} data:${data}")
         }
 
         override fun onError(platform: SHARE_MEDIA, action: Int, throwable: Throwable) {
+            Log.e(TAG, "onError ${platform} throwable:${throwable}")
         }
 
         override fun onCancel(platform: SHARE_MEDIA, action: Int) {
+            Log.i(TAG, "onCancel ${platform}")
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.i(TAG, "onActivityResult requestCode:${requestCode} resultCode:${resultCode}")
+        UMShareAPI.get(this).onActivityResult(requestCode, resultCode, data)
     }
 }
