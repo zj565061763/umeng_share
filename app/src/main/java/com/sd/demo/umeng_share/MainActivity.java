bcc7fe8da93a0a3b48295af2d778ab2f09066ada
zjun <565061763@qq.com>
2021-06-26 14:21:00 +0800

    kotlin

--- app/src/main/java/com/sd/demo/umeng_share/MainActivity.java
+++ app/src/main/java/com/sd/demo/umeng_share/MainActivity.java
@@ -4,11 +4,9 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
